e056bb567190c5119b2288563e9ea33bc52e5185
Lalit Kumar <lalitkumar1031@gmail.com>
2023-03-22 15:48:16 +0530

    Splash Screen Added

---
+++ lib/Screens/Splash_Screen.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:uiet_kuk/Screens/LoginScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 4),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen(),));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Image.asset("assets/images/uiet_logo.png",fit: BoxFit.cover)),
+    );
+  }
+}
